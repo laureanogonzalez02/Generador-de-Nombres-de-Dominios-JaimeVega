4e57be67195cd54c6b84d9e7e992b54f071f3c17
Jaime Vega <jaime.vegah@outlook.com>
2025-11-20 00:44:13 +0000

    Se termina la tarea de generador de Dominios

--- src/app.js
+++ src/app.js
@@ -8,4 +8,23 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+    let dominio = document.querySelector("#dominio");
+    let pronoun = ['the', 'our'];
+    let adj = ['great', 'big'];
+    let noun = ['jogger', 'racoon'];
+    let extensiones = ['.com', '.net', '.us', '.io'];
+
+    /* let componentes= [pronoun,adj,noun] */
+    let namePage = ''
+    pronoun.forEach(part1 => {
+      adj.forEach(part2 => {
+        noun.forEach(part3 => {
+          extensiones.forEach(ext => {
+            namePage += part1+part2+part3+ext+'<br>';
+          });
+        });
+      });
+    });
+
+    dominio.innerHTML = namePage; 
 };
